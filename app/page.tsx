cb5e2579024bc6435e3658833ed3a8cbd7b8a737
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-09-30 15:55:08 +0600

    HomePage - Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
+import './home-page.css';
+import AcmeLogo from '@/components/AcmeLogo/AcmeLogo';
+import Link from 'next/link';
+import Image from 'next/image';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <main className="main">
+      <header className="header">
+        <AcmeLogo />
+      </header>
+
+      <div className="hero-section">
+        <h1 className={`hero-title ${lusitana.className}`}>Welcome to Acme</h1>
+        <p className="hero-description">
+          This is the example for the{' '}
+          <a href="https://nextjs.org/learn/" className="learn-course-link">
+            Next.js Learn Course
+          </a>
+          , brought to you by Vercel.
+        </p>
+        <Link className="login-btn" href="/login">
+          <span className="login-text">Log in</span>
+          <ArrowRightIcon className="arrow-right-icon" />
+        </Link>
+      </div>
+
+      <div className="app-view-img-container">
+        <Image
+          className="app-view-img app-view-desktop"
+          src="/hero-desktop.png"
+          width={2000}
+          height={1520}
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+        <Image
+          className="app-view-img app-view-mobile"
+          src="/hero-mobile.png"
+          width={560}
+          height={620}
+          alt="Screenshots of the dashboard project showing mobile version"
+        />
+      </div>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,54 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-in-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'fade-in-right': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'move-left-right': {
+          '0%, 100%': {
+            transform: 'translateX(0)',
+          },
+          '50%': {
+            transform: 'translateX(5px)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.75s ease-in-out',
+        'fade-in-left': 'fade-in-left 0.75s ease-in-out',
+        'fade-in-right': 'fade-in-right 0.75s ease-in-out',
+        'move-left-right': 'move-left-right 1s infinite',
+      },
+    },
   },
   plugins: [],
 };
